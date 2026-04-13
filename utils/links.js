1258fa853d99ed5981a5b35e5e97cef228497d1e
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-14 08:47:16 +1000

    Delete utils/links.js

--- utils/links.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-export function getLinkedChannels(channelId) {
-  const config = JSON.parse(fs.readFileSync('./config.json'));
-  return config.linkedChannels[channelId] || [];
-}
