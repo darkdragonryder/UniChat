6d39804dc057e79701ad8210a3531e7bf08cef1a
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-14 08:43:49 +1000

    Create translate.js

---
+++ utils/translate.js
@@ -0,0 +1,14 @@
+export async function translate(text, targetLang) {
+  try {
+    const res = await fetch(
+      `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=en|${targetLang}`
+    );
+
+    const data = await res.json();
+
+    return data.responseData.translatedText || text;
+  } catch (err) {
+    console.error('Translation error:', err);
+    return text;
+  }
+}
