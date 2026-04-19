4a0f1574809373ae6532115e0d2c02da69c6fde8
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-19 21:45:22 +1000

    Update supabase.js

--- db/supabase.js
+++ db/supabase.js
@@ -3,7 +3,12 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl) throw new Error("SUPABASE_URL is missing");
-if (!supabaseKey) throw new Error("SUPABASE_SERVICE_ROLE_KEY is missing");
+if (!supabaseUrl) {
+  throw new Error("SUPABASE_URL is missing");
+}
+
+if (!supabaseKey) {
+  throw new Error("SUPABASE_SERVICE_ROLE_KEY is missing");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
