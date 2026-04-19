333c273714036d075a5a065aa36f69735baab587
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-19 23:15:08 +1000

    Import syncGuildLicenseExpiry from licenseExpirySync

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Client, GatewayIntentBits, Collection } from 'discord.js';
+import { syncGuildLicenseExpiry } from './services/licenseExpirySync.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
