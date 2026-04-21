bb3ab2895b47c08bed923255e5e900c617002120
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-21 20:00:12 +1000

    Add translateText function for DeepL API integration
    Implement a function to translate text using DeepL API.

---
+++ services/translator.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = process.env.DEEPL_API_KEY;
+
+export async function translateText(text, targetLang = 'EN') {
+
+  try {
+
+    const params = new URLSearchParams();
+    params.append('auth_key', API_KEY);
+    params.append('text', text);
+    params.append('target_lang', targetLang);
+
+    const res = await axios.post(
+      'https://api-free.deepl.com/v2/translate',
+      params
+    );
+
+    return res.data.translations[0].text;
+
+  } catch (err) {
+    console.error(err.message);
+    return text;
+  }
+}
