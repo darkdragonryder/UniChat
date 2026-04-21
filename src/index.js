098c7d35d25419e3d251f127d78e793af03fc44d
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-21 22:31:09 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+// src/index.js
+import "dotenv/config";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import messageCreate from "./events/messageCreate.js";
+import messageReactionAdd from "./events/messageReactionAdd.js";
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", messageCreate(client));
+client.on("messageReactionAdd", messageReactionAdd(client));
+
+client.login(process.env.DISCORD_TOKEN);
