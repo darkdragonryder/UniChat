6e47acbf1f9afc3bf57bd9084c0ad29b39d6a9bd
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-21 22:30:39 +1000

    Update detectLang.js

--- src/utils/detectLang.js
+++ src/utils/detectLang.js
@@ -1,1 +1,7 @@
-
+// src/utils/detectLang.js
+export function detectLang(text) {
+  if (/[\u3040-\u30ff]/.test(text)) return "JA";
+  if (/[\uac00-\ud7af]/.test(text)) return "KO";
+  if (/[а-яА-Я]/.test(text)) return "RU";
+  return "EN";
+}
