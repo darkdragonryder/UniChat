f043ddcb6741aba16591cd2eb4877801f24b184c
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-22 23:43:21 +1000

    Update DeepL API endpoint and authorization method

--- src/services/deepl.js
+++ src/services/deepl.js
@@ -2,19 +2,19 @@
 import axios from "axios";
 
 export async function translateText(text, targetLang) {
-  try {
-    const res = await axios.post(
-      "https://api.deepl.com/v2/translate",
-      new URLSearchParams({
-        auth_key: process.env.DEEPL_KEY,
-        text,
-        target_lang: targetLang.toUpperCase()
-      })
-    );
+  const res = await axios.post(
+    "https://api-free.deepl.com/v2/translate",
+    new URLSearchParams({
+      text,
+      target_lang: targetLang
+    }),
+    {
+      headers: {
+        Authorization: `DeepL-Auth-Key ${process.env.DEEPL_API_KEY}`,
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    }
+  );
 
-    return res.data.translations[0].text;
-  } catch (err) {
-    console.error("DeepL error:", err.message);
-    return text;
-  }
+  return res.data.translations[0].text;
 }
