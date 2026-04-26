2a62ba7e333cea441396d4cbbff4822c97bc1294
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-27 08:34:44 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { translateCached } from "./services/cacheTranslate.js";
 import setupCommand from "./commands/setup.js";
 import uninstallCommand from "./commands/uninstall.js";
 import setLanguageCommand from "./commands/setlanguage.js";
+import helpCommand from "./commands/help.js";
+import infoCommand from "./commands/info.js";
 
 const client = new Client({
   intents: [
