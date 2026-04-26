c12bec60aa5c88893a8abdfba8aefb5c7184c579
darkdragonryder <134503128+darkdragonryder@users.noreply.github.com>
2026-04-26 15:59:09 +1000

    Update detectLang.js

--- src/utils/detectLang.js
+++ src/utils/detectLang.js
@@ -1,9 +1,12 @@
-// src/utils/detectLang.js
-function guessLanguage(text) {
+export function detectLang(text) {
+  if (!text) return "EN";
+
+  if (/[\u3040-\u30ff]/.test(text)) return "JA"; // Japanese
   if (/[àèìòù]/i.test(text)) return "IT";
   if (/[äöüß]/i.test(text)) return "DE";
   if (/[ñ¿¡]/i.test(text)) return "ES";
   if (/[\u3131-\uD79D]/.test(text)) return "KO";
-  if (/[а-яА-ЯЁё]/.test(text)) return "RU";
+  if (/[а-яА-ЯЁё]/i.test(text)) return "RU";
+
   return "EN";
 }
